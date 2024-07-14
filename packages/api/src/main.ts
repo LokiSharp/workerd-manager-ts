e915ace7e8a0bbfc141bac95052004473a6130b4
LokiSharp <loki.sharp@gmail.com>
2024-07-14 22:58:50 +0800

    refactor: package utils to common

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
-import utils from '@workerd-manager/utils';
+import common from '@workerd-manager/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(utils.appConfigInstance.APIPort, utils.appConfigInstance.APIListenAddress);
+    await app.listen(common.appConfigInstance.APIPort, common.appConfigInstance.APIListenAddress);
 }
 bootstrap();
