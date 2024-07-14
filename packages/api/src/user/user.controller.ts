b5cae74a172e9f0a4ef3641bfdc4f54b31c4a11a
LokiSharp <loki.sharp@gmail.com>
2024-07-14 21:48:42 +0800

    feat: implement user CUDR

---
+++ packages/api/src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { User as UserModel, Prisma } from '@prisma/client';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly UserService: UserService) { }
+    @Post()
+    async create(@Body() UserData: Prisma.UserCreateInput): Promise<UserModel> {
+        return this.UserService.createUser(UserData);
+    }
+
+    @Put('/:id')
+    update(@Param('id') id: string, @Body() UserData: Prisma.UserUpdateInput): Promise<UserModel> {
+        return this.UserService.updateUser({
+            where: { UID: id },
+            data: UserData,
+        });
+    }
+
+    @Get()
+    findAll() {
+        return this.UserService.findUsers({});
+    }
+
+    @Get('/:id')
+    findOne(@Param('id') id: string) {
+        return this.UserService.findUser({ UID: id });
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') id: string): Promise<UserModel> {
+        return this.UserService.removeUser({ UID: id });
+    }
+}
