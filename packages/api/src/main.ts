2b17495671855550812586f224a3caa05389198b
LokiSharp <loki.sharp@gmail.com>
2024-07-17 07:39:18 +0000

    refactor: Migrate from Express to Fastify

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { ConfigService } from '@nestjs/config';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter()
+    );
     const configService = app.get(ConfigService);
     await app.listen(configService.get('API_PORT'), configService.get('API_LISTEN_ADDR'));
 }
