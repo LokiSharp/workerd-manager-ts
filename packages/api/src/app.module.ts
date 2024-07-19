4dc5aa28254f7581ba64b3d047afb6ed709e91a5
LokiSharp <loki.sharp@gmail.com>
2024-07-19 16:16:23 +0000

    feat: Use security packages

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -7,10 +7,43 @@ import { WorkerdModule } from './workerd/workerd.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-    imports: [ConfigModule.forRoot({isGlobal: true}), UsersModule, WorkerModule, WorkerdModule, AuthModule, RolesModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        UsersModule,
+        WorkerModule,
+        WorkerdModule,
+        AuthModule,
+        RolesModule,
+        ThrottlerModule.forRoot([
+            {
+                name: 'short',
+                ttl: 1000,
+                limit: 5,
+            },
+            {
+                name: 'medium',
+                ttl: 10000,
+                limit: 20
+            },
+            {
+                name: 'long',
+                ttl: 60000,
+                limit: 100
+            }
+        ]),
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        }
+
+    ],
 })
-export class AppModule {}
+export class AppModule { }
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from '@/app.module';
 import { ConfigService } from '@nestjs/config';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from '@fastify/helmet';
+import fastifyCsrf from '@fastify/csrf-protection';
+
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
         AppModule,
         new FastifyAdapter()
     );
     const configService = app.get(ConfigService);
+    await app.register(helmet)
+    await app.register(fastifyCsrf);
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(configService.get('API_PORT'), configService.get('API_LISTEN_ADDR'));
 }
